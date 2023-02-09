49c73b1d6925931514ba73f120f5900848a0a3ec
sirin <s182211@outlook.com>
2023-02-09 19:10:01 +0100

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,42 +1,36 @@
+print("Welcome to the calculator!")
+
+try:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operator = input("Enter an operator (+, -, *, /): ")
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        result = num1 / num2
+    else:
+        print("Invalid operator.")
+        result = None
+
+    if result is not None:
+        print("Result:", result)
+
+except ValueError:
+    print("Invalid input: not a number.")
+
+"""
+We can also create methods for each operation:
 def add(x, y):
     return x + y
 
 
 def multip(x, y):
     return x * y
-
-
-print("Welcome to the calculator!")
-num1 = int(input("Type the first number:"))
-num2 = int(input("Type the second number:"))
-
-print("What do you want to do with these numbers?")
-print("1. Add")
-print("2. Multiply")
-
-choice = int(input("Enter your choice (1 or 2): "))
-
 """
-# Alternative1
-if choice == 1:
-    print(num1+num2)
-elif choice == 2:
-    print(num1*num2)
-else:
-    print("Something went wrong, please try again!")
-    result = None
-"""
-
-# Alternative2
-if choice == 1:
-    result = add(num1, num2)
-elif choice == 2:
-    result = multip(num1, num2)
-else:
-    print("Invalid choice")
-    result = None
-
-if result is not None:
-    print("The result is:", result)
 
 exit()
