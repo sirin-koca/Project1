8a70f5cebdb8d153f58b7033e1f73bd9ef0f602f
sirin <s182211@outlook.com>
2023-02-25 01:20:53 +0100

    using multiplication sign inside the print statement

--- shape_of_hearth.py
+++ shape_of_hearth.py
@@ -1,3 +1,5 @@
+# This is how we print in Python:
+# We can use multiplication sign inside the print statement:
 print(" * " + "    " + " * ")
 print("*" * 4 + "  " + "*" * 4)
 print("*" * 3 + "    " + "*" * 3)
