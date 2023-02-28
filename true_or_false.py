c8c9b27098d65b014a157406dfa4964a3638657e
sirin <s182211@outlook.com>
2023-02-28 23:26:35 +0100

    If-statements, Boolean expressions

---
+++ true_or_false.py
@@ -0,0 +1,35 @@
+
+print(3 ** 2 + 1 != 30 / 3)
+
+print(3 * 2 + 1 != 30 / 3)
+
+print(3 != 3)
+
+print(3 != 4)
+
+print(3 == 3)
+
+fname = True
+sname = False
+
+if fname:
+    print("firstname")
+if sname:
+    print("surname")
+else:
+    print(None)
+
+# Output:
+""" 
+False
+True
+False
+True
+True
+firstname
+None
+"""
+
+exit()
+
+
