7d98658051f43b041328a686dca0cc1156885989
sirin <s182211@outlook.com>
2023-02-28 22:49:41 +0100

    If-statements, Boolean expressions

--- if_statements.py
+++ if_statements.py
@@ -1,4 +1,5 @@
-# If-statements
+
+# If-statements and Boolean expressions:
 
 # ex1
 test_value = 100
@@ -32,9 +33,40 @@ print("*" * 10)
 uname = input("Hello, what is your name?")
 if uname == "Dave":
     print("Get off my computer Dave!")
+    print("*" * 10)
 
 is_raining = True
 if is_raining:
     print("It is going to rain today. Bring an umbrella! ")
+    print("*" * 10)
+
+age = 12
+if age <= 13:
+    print("You are underage, and parental control required")
+    print("*" * 10)
+
+# *** AND / OR ***
+student_credits = 118
+gpa = 2.0
+if student_credits >= 120 or gpa >= 2.0:
+    print("You have met at least one of the requirements.")
+    print("*" * 10)
+
+if pet == "dog" and name == "Maya":
+    print("I love you, Maya!")
+
+
+# *** IF NOT ***
+s_credits = 120
+gpa = 1.8
+
+if not s_credits >= 120:
+    print("You do not have enough credits to graduate.")
+
+if not gpa >= 2.0:
+    print("Your GPA is not high enough to graduate.")
+
+if not s_credits >= 120 and not gpa >= 2.0:
+    print("You do not meet either requirement to graduate!")
 
 exit()
