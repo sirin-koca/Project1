6774b97135d55008360bca8252252c62e30a0c23
sirin <s182211@outlook.com>
2023-03-01 15:16:53 +0100

    Print string by index number

---
+++ programs/python.py
@@ -0,0 +1,14 @@
+def print_python():
+    lang = "PYTHON"
+    print(" " + lang[0] + " * " * 1)
+    print(" " + lang[1] + " * " * 2)
+    print(" " + lang[2] + " * " * 3)
+    print(" " + lang[3] + " * " * 4)
+    print(" " + lang[4] + " * " * 5)
+    print(" " + lang[5] + " * " * 6)
+    return lang
+
+
+print_python()
+
+exit()
