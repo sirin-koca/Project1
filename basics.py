3d296f0699bf94f0a406e899157ac9ef27000a6c
sirin <s182211@outlook.com>
2023-03-04 19:48:56 +0100

    Basics of Python

--- basics.py
+++ basics.py
@@ -2,6 +2,23 @@
 # Comment on a single line
 user = "JDoe"  # Comment after code
 
+"""
+This is a
+multiline
+comment.
+"""
+
+# Concatenation
+"""
+In Python, we cannot concatenate a string and an integer directly because they have different data types. 
+We need to convert the integer to a string first. You can do this by using the str() function to convert 
+the integer to a string, and then you can concatenate the two values. Here is an example:
+"""
+x = 25
+y = "The lucky number is: "
+z = y + str(x)
+print(z)
+
 # Arithmetic operations
 result1 = 10 + 30
 result2 = 40 - 10
@@ -9,92 +26,64 @@ result3 = 50 * 5
 result4 = 16 / 4
 result5 = 25 % 2
 result6 = 5 ** 3
+print("Arithmetic operations: ")
 print(result1, result2, result3, result4, result5, result6)
 
-# Concatenation
-"""
-In Python, you cannot concatenate a string and an integer directly because they have different data types. 
-We need to convert the integer to a string first. You can do this by using the str() function to convert 
-the integer to a string, and then you can concatenate the two values. Here is an example:
-"""
-x = 42
-y = "The answer is: "
-z = y + str(x)
-print(z)
-
 # Plus-Equal Operator
-
 counter = 0
 counter += 10
 
 # This is equivalent to
-
 counter = 0
 counter = counter + 10
 
-# The operator will also perform string concatenation
-
-message = "Part 1 of message "
-message += "Part 2 of message"
-
 # These are all valid variable names and assignment
-
 user_name = "codey"
 user_id = 100
 verified = False
 
 # A variable's value can be changed after assignment
-
 points = 100
 points = 120
 
 # Modulo operations
-
 zero = 8 % 4
 
 nonzero = 12 % 5
 
 # Example integer numbers
-
 chairs = 4
 tables = 1
 broken_chairs = -2
 sofas = 0
 
 # Non-integer numbers
-
 lights = 2.5
 left_overs = 0.0
 
 # String concatenation
-
 first = "Hello "
 second = "World"
-
 result = first + second
+print(result)
 
-long_result = first + second + "!"
+# The + operator will also perform string concatenation
+message = "Part 1 of message "
+message += "Part 2 of message"
 
 numerator = 100
 denominator = 0
 # bad_results = numerator / denominator  # ZeroDivisionError: division by zero
 
-user = "User Full Name"
+users = "User Full Name"
 game = 'Monopoly'
-
 longer = "This string is broken up \
 over multiple lines"
 
 # SyntaxError: can't assign to operator
 # age = 7 + 5 = 4
 
-
-print("Hello World!")
-
-print(100)
-
 # Floating point numbers
-
 pi = 3.14159
 meal_cost = 12.99
 tip_percent = 0.20
