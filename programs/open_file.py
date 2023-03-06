f66a8ef5f6829952b26b1dae983b7886a061e8c1
sirin <s182211@outlook.com>
2023-03-06 19:58:12 +0100

    open a txt file with os-module

--- programs/open_file.py
+++ programs/open_file.py
@@ -1,14 +1,11 @@
-# Import os-module:
 import os
 
-# Show current directory and its items:
 cwd = os.getcwd()
 ls = os.listdir()
 
 print("Current working directory:", cwd)
 print("Items in this directory:", ls)
 
-# Get user input for file name to open:
 name = input("Enter file name: ")
 
 
@@ -28,4 +25,3 @@ if file:
     print(content)
 else:
     print("File not found")
-
