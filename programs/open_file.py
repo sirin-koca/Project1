7e9af73eebcf6458d7b6209a7974dfbcae8e8bcc
sirin <s182211@outlook.com>
2023-03-06 19:34:34 +0100

    open file with os-module

--- programs/open_file.py
+++ programs/open_file.py
@@ -1,11 +1,14 @@
+# Import os-module:
 import os
 
+# Show current directory and its items:
 cwd = os.getcwd()
 ls = os.listdir()
 
 print("Current working directory:", cwd)
 print("Items in this directory:", ls)
 
+# Get user input for file name to open:
 name = input("Enter file name: ")
 
 
