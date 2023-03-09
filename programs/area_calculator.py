8848b8be3a617d9e49e660b9d6e575cf8b89c014
sirin <s182211@outlook.com>
2023-03-09 18:46:49 +0100

    basic area calculations and f-string

---
+++ programs/area_calculator.py
@@ -0,0 +1,20 @@
+# Area Calculator 📏
+
+import math
+import sys
+
+base = 20
+height = 30
+area = (base * height) / 2
+print(f'The triangle area is {area}')
+
+length = 2
+width = 12
+area = length * width
+print(f'The rectangle area is {area}')
+
+radius = 36
+area = round(math.pi * radius ** 2)
+print(f'The circle area is {area}')
+
+sys.exit()
