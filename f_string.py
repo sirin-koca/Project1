458d8348eb9f315d47a0d66c6d6af8b9b3f95590
sirin <s182211@outlook.com>
2023-03-12 23:59:08 +0100

    f-string in Python

---
+++ f_string.py
@@ -0,0 +1,10 @@
+import sys
+
+name = "Alice"
+age = 30
+height = 1.75
+
+# Example of using f-string in Python:
+print(f"My name is {name}, I am {age} years old, and my height is {height:.2f} meters.")
+
+sys.exit()
